5d6ef63aa87dac916775cd84f00eaecfbd602a24
chsharmila <47497710+chsharmila@users.noreply.github.com>
2020-01-28 16:42:42 +0530

    Add files via upload

---
+++ P1one.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class P1one{
+     public static void main(String args[]){  
+System.out.println(args[0]+"  Technologies  "+args[1]);
+
+  
+}  
+}  
---
+++ P2one.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class P2one{
+     public static void main(String args[]){  
+ int x =Integer.parseInt(args[0]);
+
+     if(x>0)
+{
+System.out.println(args[0]+" is positive");
+}else if(x<0)
+{
+System.out.println(args[0]+" is negative");
+}
+else 
+{
+System.out.println(x+" is neither positive nor negative");
+}
+}
+}
